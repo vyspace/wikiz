aa9d9cae27929528e014dac9c28ab7a07b32850d
zh <zh@TheiMac.local>
2017-03-31 18:04:38 +0800

    reconstituting some css

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 (function($, undefined){
 	"use strict"
+	var menuSwitch = $("#menuSwitch"),
+		subMenuOpen = $("#subMenuOpen"),
+		navMenu = $("#navMenu");
 	$("#myCarousiel").carousel();
+	menuSwitch.on("click", function(e) {
+		var t = $(this),
+			exp = t.attr("aria-expanded");
+		if(exp == "true") {
+			t.removeClass("nav-open");
+		}
+		else {
+			t.addClass("nav-open");
+		}
+	});
+	subMenuOpen.on("click", function(e) {
+		e.stopPropagation();
+		navMenu.addClass("move-in");
+	});
 })(jQuery);
